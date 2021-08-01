54ab596407fb5706b1a72015d16ebeaaf4719db6
sreejithan86 <sreejithan86@gmail.com>
2021-08-01 19:43:57 +0530

    pom files and test

---
+++ src/main/java/com/springboot/springbootfisrtapp/SpringbootfisrtAppApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.springbootfisrtapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootfisrtAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootfisrtAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/springbootfisrtapp/SpringbootfisrtAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.springbootfisrtapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootfisrtAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
